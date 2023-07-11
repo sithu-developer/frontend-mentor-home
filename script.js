5a7c80c73a5a27dd450041c7e6b49930b8eab732
Sithu Naing <sithunaing8209@gmail.com>
2023-07-11 15:32:07 +0630

    first

---
+++ script.js
@@ -0,0 +1,16 @@
+let bodyTag = document.getElementsByTagName("body")[0];
+let webImgTag = document.querySelector(".webImg");
+let containerOfLi = document.querySelector(".containerOfLi");
+let bar1 = document.querySelector(".bar1");
+let bar2 = document.querySelector(".bar2");
+let bar3 = document.querySelector(".bar3");
+
+if (bodyTag.style.width < 400) {
+    webImgTag.src = "assets/images/image-web-3-mobile.jpg";
+
+}
+
+
+containerOfLi.addEventListener("click", () => {
+    
+})
